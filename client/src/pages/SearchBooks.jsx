1c7dd7dbcbc1c358eb73208b26f4cbd352c07361
kylezap <kzapcic+github@gmail.com>
2024-06-10 14:38:14 -0400

    modify card images to better fit ui

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -1,36 +1,32 @@
-import { useState, useEffect } from 'react';
-import {
-  Container,
-  Col,
-  Form,
-  Button,
-  Card,
-  Row
-} from 'react-bootstrap';
+import { useState, useEffect } from "react";
+import { Container, Col, Form, Button, Card, Row } from "react-bootstrap";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 // import { saveBook, searchGoogleBooks } from '../utils/API';
-import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-import { useLazyQuery, useMutation } from '@apollo/client';
-import { SEARCH_BOOKS } from '../utils/queries';
-import { SAVE_BOOK } from '../utils/mutations';
+import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
+import { useLazyQuery, useMutation } from "@apollo/client";
+import { SEARCH_BOOKS } from "../utils/queries";
+import { SAVE_BOOK } from "../utils/mutations";
 
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
-  const [searchInput, setSearchInput] = useState('');
+  const [searchInput, setSearchInput] = useState("");
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  const [searchBooks] = useLazyQuery(SEARCH_BOOKS, {onCompleted: data => setSearchedBooks(data.searchBooks)});
- 
+  const [searchBooks] = useLazyQuery(SEARCH_BOOKS, {
+    onCompleted: (data) => setSearchedBooks(data.searchBooks),
+  });
+
   console.log(savedBookIds);
-  
+
   // create state to hold our saved book data
   const [saveBook] = useMutation(SAVE_BOOK, {
-    onCompleted: data => setSavedBookIds([...savedBookIds, data.saveBook.bookId]),
+    onCompleted: (data) =>
+      setSavedBookIds([...savedBookIds, data.saveBook.bookId]),
   });
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
@@ -48,7 +44,7 @@ const SearchBooks = () => {
     }
     try {
       await searchBooks({ variables: { title: searchInput } });
-      setSearchInput('');
+      setSearchInput("");
     } catch (err) {
       console.error(err);
     }
@@ -99,7 +95,7 @@ const SearchBooks = () => {
     }
 
     //commented out to use Apollo
-    
+
     // try {
     //   const response = await saveBook(bookToSave, token);
 
@@ -123,16 +119,16 @@ const SearchBooks = () => {
             <Row>
               <Col xs={12} md={8}>
                 <Form.Control
-                  name='searchInput'
+                  name="searchInput"
                   value={searchInput}
                   onChange={(e) => setSearchInput(e.target.value)}
-                  type='text'
-                  size='lg'
-                  placeholder='Search for a book'
+                  type="text"
+                  size="lg"
+                  placeholder="Search for a book"
                 />
               </Col>
               <Col xs={12} md={4}>
-                <Button type='submit' variant='success' size='lg'>
+                <Button type="submit" variant="success" size="lg">
                   Submit Search
                 </Button>
               </Col>
@@ -142,31 +138,41 @@ const SearchBooks = () => {
       </div>
 
       <Container>
-        <h2 className='pt-5'>
+        <h2 className="pt-5">
           {searchedBooks.length
             ? `Viewing ${searchedBooks.length} results:`
-            : 'Search for a book to begin'}
+            : "Search for a book to begin"}
         </h2>
         <Row>
           {searchedBooks.map((book) => {
             return (
               <Col md="4" key={book.bookId}>
-                <Card border='dark'>
+                <Card border="dark">
                   {book.image ? (
-                    <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />
+                    <Card.Img
+                      src={book.image}
+                      alt={`The cover for ${book.title}`}
+                      variant="top"
+                      style={{ width: "100%", height: "20vw", } }
+                    />
                   ) : null}
                   <Card.Body>
                     <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
+                    <p className="small">Authors: {book.authors}</p>
                     <Card.Text>{book.description}</Card.Text>
                     {Auth.loggedIn() && (
                       <Button
-                        disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
-                        className='btn-block btn-info'
-                        onClick={() => handleSaveBook(book.bookId)}>
-                        {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
-                          ? 'This book has already been saved!'
-                          : 'Save this Book!'}
+                        disabled={savedBookIds?.some(
+                          (savedBookId) => savedBookId === book.bookId
+                        )}
+                        className="btn-block btn-info"
+                        onClick={() => handleSaveBook(book.bookId)}
+                      >
+                        {savedBookIds?.some(
+                          (savedBookId) => savedBookId === book.bookId
+                        )
+                          ? "This book has already been saved!"
+                          : "Save this Book!"}
                       </Button>
                     )}
                   </Card.Body>
